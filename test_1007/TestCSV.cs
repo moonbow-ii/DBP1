f8dadc769d923f9ec0e52deebe96d281d8aa3615
5532528 <5532528@stu.kmu.ac.kr>
2020-10-07 22:03:49 +0900

    TestCSV v0.3 201008

--- test_1007/TestCSV.cs
+++ test_1007/TestCSV.cs
@@ -21,19 +21,21 @@ namespace test_1007
         private void buttonOpenCSV_Click(object sender, EventArgs e)
         {
             StreamReader sr = new StreamReader(new FileStream("market.csv", FileMode.Open)
-                , Encoding.Default);
-            var str = "blank";
+            , Encoding.Default);
+            var str = "";
             var line = sr.ReadLine();
-   
+
             while (sr.EndOfStream == false)
             {
                 var values = line.Split(',');
-                foreach (String value in values)
+                foreach (String string_value in values)
                 {
                     line = sr.ReadLine();
-                    str = value;
-                    CSV_text.Text += str.PadRight(30-value.Length);
+                    str = string_value;
+                    CSV_text.Text += str.PadRight(30 - string_value.Length);
                 }
+                Console.WriteLine("입력완료");
+
                 CSV_text.Text += "\r\n";
             }
             sr.Close();
