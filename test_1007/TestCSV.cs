041ad157f5c9e30b963bdb71c6dd62c9aacfb395
moonbow-ii <68647614+moonbow-ii@users.noreply.github.com>
2020-10-08 01:20:39 +0900

    Update TestCSV.cs

--- test_1007/TestCSV.cs
+++ test_1007/TestCSV.cs
@@ -36,7 +36,7 @@ namespace test_1007
                 }
                 Console.WriteLine("입력완료");
                 CSV_text.Text += "\r\n";
-            }//merge_test
+            }//conflict_test
             sr.Close();
         }
     }
