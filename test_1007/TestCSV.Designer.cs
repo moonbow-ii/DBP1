1d077ca649f2a4438abbf8b091eeb912fc32f527
moonbow-ii <un9011@gmail.com>
2020-10-14 17:58:43 +0900

    Column Search

--- test_1007/TestCSV.Designer.cs
+++ test_1007/TestCSV.Designer.cs
@@ -30,11 +30,17 @@
         {
             this.buttonOpenCSV = new System.Windows.Forms.Button();
             this.CSV_text = new System.Windows.Forms.TextBox();
+            this.listBoxHeaders = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSearchKeyword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSearchResult = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonOpenCSV
             // 
-            this.buttonOpenCSV.Location = new System.Drawing.Point(327, 398);
+            this.buttonOpenCSV.Location = new System.Drawing.Point(12, 12);
             this.buttonOpenCSV.Name = "buttonOpenCSV";
             this.buttonOpenCSV.Size = new System.Drawing.Size(140, 40);
             this.buttonOpenCSV.TabIndex = 0;
@@ -48,21 +54,84 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.CSV_text.Font = new System.Drawing.Font("굴림", 8F);
-            this.CSV_text.Location = new System.Drawing.Point(12, 12);
+            this.CSV_text.Location = new System.Drawing.Point(158, 302);
             this.CSV_text.Multiline = true;
             this.CSV_text.Name = "CSV_text";
-            this.CSV_text.Size = new System.Drawing.Size(776, 380);
+            this.CSV_text.Size = new System.Drawing.Size(682, 190);
             this.CSV_text.TabIndex = 1;
+            // 
+            // listBoxHeaders
+            // 
+            this.listBoxHeaders.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBoxHeaders.FormattingEnabled = true;
+            this.listBoxHeaders.ItemHeight = 12;
+            this.listBoxHeaders.Location = new System.Drawing.Point(12, 95);
+            this.listBoxHeaders.Name = "listBoxHeaders";
+            this.listBoxHeaders.Size = new System.Drawing.Size(140, 400);
+            this.listBoxHeaders.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "컬럼 헤더 리스트";
+            // 
+            // textBoxSearchKeyword
+            // 
+            this.textBoxSearchKeyword.Location = new System.Drawing.Point(164, 41);
+            this.textBoxSearchKeyword.Name = "textBoxSearchKeyword";
+            this.textBoxSearchKeyword.Size = new System.Drawing.Size(152, 21);
+            this.textBoxSearchKeyword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "검색 키워드";
+            // 
+            // textBoxSearchResult
+            // 
+            this.textBoxSearchResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSearchResult.Font = new System.Drawing.Font("굴림", 8F);
+            this.textBoxSearchResult.Location = new System.Drawing.Point(158, 154);
+            this.textBoxSearchResult.Multiline = true;
+            this.textBoxSearchResult.Name = "textBoxSearchResult";
+            this.textBoxSearchResult.Size = new System.Drawing.Size(682, 139);
+            this.textBoxSearchResult.TabIndex = 1;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(164, 68);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(152, 62);
+            this.buttonSearch.TabIndex = 5;
+            this.buttonSearch.Text = "검색하기";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
             // 
             // TestCSV
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(852, 530);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxSearchKeyword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxHeaders);
+            this.Controls.Add(this.textBoxSearchResult);
             this.Controls.Add(this.CSV_text);
             this.Controls.Add(this.buttonOpenCSV);
             this.Name = "TestCSV";
-            this.Text = "Form1";
+            this.Text = "CSV 검색기";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,6 +141,12 @@
 
         private System.Windows.Forms.Button buttonOpenCSV;
         private System.Windows.Forms.TextBox CSV_text;
+        private System.Windows.Forms.ListBox listBoxHeaders;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSearchKeyword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSearchResult;
+        private System.Windows.Forms.Button buttonSearch;
     }
 }
 
--- test_1007/TestCSV.cs
+++ test_1007/TestCSV.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Management.Instrumentation;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,6 +26,7 @@ namespace test_1007
         //버튼을 통해서 csv파일 불러오기
         private List<List<string>> MakeColumnarDataStructure()
         {
+
             StreamReader sr = new StreamReader(new FileStream("market.csv", FileMode.Open)
            , Encoding.Default);
 
@@ -43,8 +48,7 @@ namespace test_1007
             while (sr.EndOfStream == false)
             {
                 line = sr.ReadLine();
-                var value = line.Split(',');
-                value = parseCSVLine(value);
+                var value = parseCSVLine(line);
                 for (int i = 0; i < value.Length; i++)
                     wholeData[i].Add(value[i]);
             }
@@ -71,36 +75,52 @@ namespace test_1007
             }
             CSV_text.Text = str;
         }
-            
-        private List<List<string>> MakeRowbaseDataStructure()
+
+        Dictionary<string, int> headerIndexDic = new Dictionary<string, int>();
+
+        private void setHeaderList(string header)
         {
-            StreamReader sr = new StreamReader(new FileStream("market.csv", FileMode.Open)
+            var values = parseCSVLine(header);
+
+            listBoxHeaders.Items.Clear();
+
+            for(int i=0; i < values.Length; i++){
+                string column = values[i];
+                listBoxHeaders.Items.Add(column);
+                headerIndexDic[column] = i;
+            }            
+        }
+        List<List<string>> wholeData;
+        private List<List<string>> MakeRowbaseDataStructure(string filename)
+        {
+            StreamReader sr = new StreamReader(new FileStream(filename, FileMode.Open)
            , Encoding.Default);
 
-            List<List<string>> wholeData = new List<List<string>>();
-
             var line = sr.ReadLine();
+            setHeaderList(line);
+
+
+            wholeData = new List<List<string>>();
 
             while (sr.EndOfStream == false)
             {
                 line = sr.ReadLine(); 
-                var value = line.Split(',');
-                value = parseCSVLine(value);
+                var value = parseCSVLine(line);
                 wholeData.Add(value.ToList());//string 배열을 리스트 형태로 넣음
             }
 
             sr.Close();
             return wholeData;
         }
-        private void printRowData_R(List<List<string>> data) {
+        private void printRowData_R(List<List<string>> data, TextBox textBox) {
             string str = "";
-            CSV_text.Text = "";
+            textBox.Text = "";
             foreach(List<string> values in data){
                 foreach (string value in values)
                     str += value + '\t';
-                str += '\n';
+                str += "\r\n";
             }
-            CSV_text.Text = str;
+            textBox.Text = str;
         }
         private void printColumnData_R(List<List<string>> data, int colIdx) {
             string str = "";
@@ -111,26 +131,77 @@ namespace test_1007
             CSV_text.Text = str;
         }//coInx에 숫자 넣으면 그 부분만 나오는
 
-        private string[] parseCSVLine(string[] line) {        
-             for(int j=0; j<line.Length; j++) {
-                if (line[j][0] == '\"'){
-                    line[j] = line[j] + ',' + line[j+1];
-                    for (int i = j+1; i < line.Length - 1; i++)
-                        line[i] = line[i + 1];
-                    Array.Resize(ref line, line.Length - 1);
+        private string[] parseCSVLine(string value) {
+            var list =  value.Split(',');
+            for (int j=0; j<list.Length; j++) {
+                if (list[j][0] == '\"'){
+                    list[j] = list[j] + ',' + list[j+1];
+                    for (int i = j+1; i < list.Length - 1; i++)
+                        list[i] = list[i + 1];
+                    Array.Resize(ref list, list.Length - 1);
                 }
             }                       
-            return line;
+            return list;
         }// (구, 우리시장) 같은거 잘되도록.
         private void buttonOpenCSV_Click(object sender, EventArgs e){
-            List<List<string>> data_C = MakeColumnarDataStructure();
-            //List<List<string>> data_R = MakeRowbaseDataStructure();
+            var FD = new OpenFileDialog();
+            DialogResult dresult = FD.ShowDialog();
+
+            if (dresult == DialogResult.OK){
+                string filename = FD.FileName;
+                List<List<string>> data_R = MakeRowbaseDataStructure(filename);
+                printRowData_R(data_R,CSV_text);
+            }
+
+            //List<List<string>> data_C = MakeColumnarDataStructure();
+
             //int selectIndex=1;
             //printColumnData_C(data_C, selectIndex);
-            printRowData_C(data_C);
+            //printRowData_C(data_C);
             //printColumnData_R(data_R, selectIndex);
-            //printRowData_R(data_R);
 
+
+        }
+        private void doSearch(string key, string col)
+        {
+            //대상컬럼 확인
+            //int targetcolumnIndex = listBoxheaders.selectedIndex;
+            //dictionary활용
+            if (headerIndexDic.ContainsKey(col) == false)
+            {
+                MessageBox.Show("검색 컬럼을 선택하세요.", "확인");
+                return;
+            }
+            int targetcolumnIndex=headerIndexDic[col];
+
+            //대상 컬럼에서 키워드 확인
+            List<List<string>> search = new List<List<string>>(); ;
+            foreach (List<string> row in wholeData)
+            {
+                var result = row[targetcolumnIndex];
+                if (result.Contains(key))
+                    search.Add(row);
+            }
+            printRowData_R(search, textBoxSearchResult);
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = textBoxSearchKeyword.Text;
+
+            if (keyword == "")
+            {
+                MessageBox.Show("검색 키워드를 입력하세요.", "확인");
+                return;
+            }
+            if(listBoxHeaders.SelectedItem == null)
+            {
+                MessageBox.Show("검색 컬럼을 선택하세요.", "확인");
+                return;
+            }
+
+            string column = listBoxHeaders.SelectedItem.ToString();
+            doSearch(keyword, column);
         }
     }
 }
